9dc69c6464f363f645a7f66f6118f2cf388184eb
Saiban <60969163+Saiban0@users.noreply.github.com>
2025-09-25 19:41:08 +0200

    + | added shooting signal btw cue and main

--- scripts/main.gd
+++ scripts/main.gd
@@ -1,28 +1,25 @@
 extends Node
 
+const globals = preload("res://scripts/globals.gd")
+
 @export var ball_scene : PackedScene
 
 #game variables
 var ball_images := []
 var cue_ball
-const START_POS := Vector2(1152, 389.1)
-
 
 func _ready():
 	load_images()
 	new_game()
-
 
 func load_images():
 	for i in range(1, 17, 1):
 		var ball_image = load(str("res://assets/ball_", i, ".png"))
 		ball_images.append(ball_image)
 
-
 func new_game():
 	generate_balls()
 	reset_cue_ball()
-
 
 func generate_balls():
 	var ball_nb : int = 0
@@ -40,20 +37,17 @@ func generate_balls():
 			ball_nb += 1
 		rows -= 1
 
-
 func reset_cue_ball():
 	cue_ball = ball_scene.instantiate()
 	add_child(cue_ball)
-	cue_ball.position = START_POS
+	cue_ball.position = globals.START_POS
 	cue_ball.get_node("Sprite2D").texture = ball_images[-1]
-
 
 func show_cue():
 	$Cue.position = cue_ball.position
-	var mouse_pos = get_viewport().get_mouse_position()
-	var cue_dir = -(mouse_pos - cue_ball.position)
-	$Cue.rotation = cue_dir.angle()
-
 
 func _process(delta):
 	show_cue()
+
+func _on_cue_shoot(power):
+	cue_ball.apply_central_impulse(power)
