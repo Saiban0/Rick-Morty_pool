473e4635ea917ad76d805cfd6ed227c68484c5a4
Saiban <60969163+Saiban0@users.noreply.github.com>
2025-09-25 19:39:10 +0200

    + | added basic cue script

---
+++ scenes/cue.gd
@@ -0,0 +1,24 @@
+extends Sprite2D
+
+const globals = preload("res://scripts/globals.gd")
+
+signal shoot
+
+var power : float = 0.0
+var power_dir = 1
+
+func _process(delta):
+	var mouse_pos = get_viewport().get_mouse_position()
+	look_at(mouse_pos)
+	if Input.is_mouse_button_pressed(MOUSE_BUTTON_LEFT):
+		power += 0.1 * power_dir
+		if (power > globals.MAX_POWER):
+			power_dir = -1
+		elif (power <= 0):
+			power_dir = 1
+	else:
+			power_dir = 1
+			if (power > 0):
+				var dir = mouse_pos - position
+				shoot.emit(power * dir)
+				power = 0
