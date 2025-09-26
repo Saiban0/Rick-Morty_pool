78adc8fef9e42d6e548a77b574499796f224d232
Saiban <60969163+Saiban0@users.noreply.github.com>
2025-09-26 22:21:58 +0200

    + | new holes script hides the fallen balls

---
+++ scripts/holes.gd
@@ -0,0 +1,11 @@
+extends Area2D
+
+func _ready():
+	connect("body_entered", Callable(_on_body_entered))
+	
+func _on_body_entered(body):
+	if body.is_in_group("balls"):
+		body.set_physics_process(false)
+		body.hide()
+		body.set_axis_velocity(Vector2(0.0, 0.0))
+		body.sleeping = 1
