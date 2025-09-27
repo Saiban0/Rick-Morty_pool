079886a3668c5b155d9a689673fe7ab054f398b5
Saiban0 <bartched@outlook.fr>
2025-09-27 17:43:01 +0200

    +~ | modified power scaling values and added new cancel shot feature

--- scripts/cue.gd
+++ scripts/cue.gd
@@ -7,19 +7,22 @@ signal shoot
 var power : float = 0.0
 var power_dir = 1
 
-func _process(delta):
+func _process(_delta):
 	var mouse_pos = get_viewport().get_mouse_position()
 	look_at(mouse_pos)
 	if (self.visible):
 		if Input.is_mouse_button_pressed(MOUSE_BUTTON_LEFT):
-			power += 0.1 * power_dir
+			power += 0.375 * power_dir
 			if (power > globals.MAX_POWER):
 				power_dir = -1
 			elif (power <= 0):
 				power_dir = 1
+			if Input.is_mouse_button_pressed(MOUSE_BUTTON_RIGHT):
+				power = 0
+				return
 		else:
 				power_dir = 1
 				if (power > 0):
 					var dir = mouse_pos - position
-					shoot.emit(power * dir)
+					shoot.emit(power * (dir.normalized() * 25 ))
 					power = 0
--- scripts/globals.gd
+++ scripts/globals.gd
@@ -1,4 +1,4 @@
 extends Node
 
-const MAX_POWER : float = 6.0
+const MAX_POWER : float = 120.0
 const START_POS := Vector2(1152, 389.1)
