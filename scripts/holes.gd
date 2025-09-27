b5a06a6a78ea7de1914096c1b0e3c5326245685d
Saiban <60969163+Saiban0@users.noreply.github.com>
2025-09-27 15:38:22 +0200

    +~ | fixed balls still moving when holed

--- scripts/holes.gd
+++ scripts/holes.gd
@@ -7,5 +7,6 @@ func _on_body_entered(body):
 	if body.is_in_group("balls"):
 		body.set_physics_process(false)
 		body.hide()
-		body.set_axis_velocity(Vector2(0.0, 0.0))
+		body.linear_velocity = Vector2(0.0, 0.0)
+		body.angular_velocity = 0.0
 		body.sleeping = 1
