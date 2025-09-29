1266e89b9e1d8381f3da56dc6e1ee67aaf8fe869
Saiban <60969163+Saiban0@users.noreply.github.com>
2025-09-29 20:11:54 +0200

    + | team score now updates and send update scoreboard signal

--- scripts/holes.gd
+++ scripts/holes.gd
@@ -1,5 +1,7 @@
 extends Area2D
 
+signal point_scored
+
 func _ready():
 	connect("body_entered", Callable(_on_body_entered))
 	
@@ -10,3 +12,8 @@ func _on_body_entered(body):
 		body.linear_velocity = Vector2(0.0, 0.0)
 		body.angular_velocity = 0.0
 		body.sleeping = 1
+		if body.is_in_group("balls_A"):
+			Globals.team_a_score += 1
+		elif body.is_in_group("balls_B"):
+			Globals.team_b_score += 1
+		emit_signal("point_scored")
