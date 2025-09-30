74fc9d9776dd094a0cc463e6622477dda9dd1a25
Saiban <60969163+Saiban0@users.noreply.github.com>
2025-09-30 22:26:30 +0200

    ~ | Teams are now made of 2 players instead of 3

--- scripts/globals.gd
+++ scripts/globals.gd
@@ -10,15 +10,11 @@ var team_a_score : int = 0
 var team_b_score : int = 0
 var Aplayer1_name : String = "Aplayer1"
 var Aplayer2_name : String = "Aplayer2"
-var Aplayer3_name : String = "Aplayer3"
 var Bplayer1_name : String = "Bplayer1"
 var Bplayer2_name : String = "Bplayer2"
-var Bplayer3_name : String = "Bplayer3"
 var	Aplayer1_avatar = avatar_rick
 var	Aplayer2_avatar = avatar_morty
-var	Aplayer3_avatar = avatar_squanchy
-var	Bplayer1_avatar = avatar_poopy
+var	Bplayer1_avatar = avatar_squanchy
 var	Bplayer2_avatar = avatar_poopy
-var	Bplayer3_avatar = avatar_poopy
-var players : Array = [{"avatar": Aplayer1_avatar, "name": Aplayer1_name, "score": 0},{"avatar": Aplayer2_avatar, "name": Aplayer2_name, "score": 0},{"avatar": Aplayer3_avatar, "name": Aplayer3_name, "score": 0},
-				{"avatar": Bplayer1_avatar, "name": Bplayer1_name, "score": 0},{"avatar": Bplayer2_avatar, "name": Bplayer2_name, "score": 0},{"avatar": Bplayer3_avatar, "name": Bplayer3_name, "score": 0},]
+var players : Array = [{"avatar": Aplayer1_avatar, "name": Aplayer1_name, "score": 0},{"avatar": Aplayer2_avatar, "name": Aplayer2_name, "score": 0},
+				{"avatar": Bplayer1_avatar, "name": Bplayer1_name, "score": 0},{"avatar": Bplayer2_avatar, "name": Bplayer2_name, "score": 0}]
