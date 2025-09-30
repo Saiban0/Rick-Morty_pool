4f931d4d7da78ddedf328f74a48be93282355cdf
Saiban <60969163+Saiban0@users.noreply.github.com>
2025-10-01 00:00:53 +0200

    + | did some styling

--- scripts/potted_balls_display.gd
+++ scripts/potted_balls_display.gd
@@ -5,4 +5,4 @@ signal dropped_ball
 func _on_table_ball_dropped(ball) -> void:
 	dropped_ball.emit(ball)
 	$"../GameViewport/SubViewport/Main".remove_child(ball)
-	$MarginContainer/SubViewportContainer/SubViewport/Area2D/StaticBody2D.add_child(ball)
+	$SubViewportContainer/SubViewport/Area2D/StaticBody2D.add_child(ball)
