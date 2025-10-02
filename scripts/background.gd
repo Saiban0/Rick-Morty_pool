038ca36688ea442d0de793d4bbfc33ba5bbc64c2
Saiban <60969163+Saiban0@users.noreply.github.com>
2025-10-02 19:53:32 +0200

    - | removed background assets and script (will be handled by frontend)

--- scripts/background.gd
+++
@@ -1,6 +0,0 @@
-extends TextureRect
-
-func _ready() -> void:
-	var rng = RandomNumberGenerator.new()
-	var bg : Texture = load(str("res://assets/backgrounds/planet", rng.randi_range(1,10),"_bg.png"))
-	self.texture = bg
