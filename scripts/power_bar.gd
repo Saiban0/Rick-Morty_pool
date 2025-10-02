f45f3778c8e02085fcb35f903007e0cf2be04430
Saiban <60969163+Saiban0@users.noreply.github.com>
2025-10-02 19:55:08 +0200

    ~ | Adjusted small things

--- scripts/power_bar.gd
+++ scripts/power_bar.gd
@@ -1,6 +1,4 @@
 extends ProgressBar
 
-const globals = preload("res://scripts/globals.gd")
-
 func _process(_delta: float):
-	value = $"../Cue".power * (100 / globals.MAX_POWER)
+	value = $"../Cue".power * (100 / Globals.MAX_POWER)
