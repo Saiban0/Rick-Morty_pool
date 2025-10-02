d6bde894a0fedaf83f36015096ee092251bde594
Saiban <60969163+Saiban0@users.noreply.github.com>
2025-10-02 19:54:50 +0200

    + | current player now highlighted in scoreboard

--- scripts/scoreboard.gd
+++ scripts/scoreboard.gd
@@ -6,6 +6,14 @@ extends VBoxContainer
 
 func _ready():
 	set_scoreboard()
+	swap_1_and_2((name_group))
+	_set_label_color()
+
+func swap_1_and_2(array: Array) -> Array:
+	var temp = array[1]
+	array[1] = array[2]
+	array[2] = temp
+	return array
 
 func set_scoreboard():
 	var i = 0
@@ -26,3 +34,12 @@ func _update_scoreboard():
 		i += 1
 	$Score/Score.text = "%d : %d" % [Globals.team_a_score, Globals.team_b_score]
 	
+
+func _set_label_color():
+	for i in name_group.size():
+		if i == Globals.current_turn_index:
+			name_group[i].add_theme_color_override("font_color", Color(1.0, 1.0, 0.0, 1.0))
+			#score_group[i].add_theme_color_override("font_color", Color(1.0, 1.0, 0.0, 1.0))
+		else:
+			name_group[i].remove_theme_color_override("font_color")
+			#score_group[i].remove_theme_color_override("font_color")
