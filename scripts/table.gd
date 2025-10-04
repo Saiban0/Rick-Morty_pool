6be4239d7d6bc33d1baa058c226b10c658f23af9
Saiban <60969163+Saiban0@users.noreply.github.com>
2025-10-04 20:41:16 +0200

    ~ | rewritten _on_holes_point_scored and fixed winning conditions

--- scripts/table.gd
+++ scripts/table.gd
@@ -5,32 +5,34 @@ signal ball_dropped
 signal cue_ball_potted
 signal game_over
 
+func _handle_points_and_turns(team : String):
+	if Globals.turn_order[Globals.current_turn_index]["team"] == team:
+		Globals.players[Globals.current_turn_index]["score"] += 1
+		Globals.turn_order[Globals.current_turn_index]["turns"] += 1
+	else:
+		Globals.turn_order[(Globals.current_turn_index + 1) % Globals.players.size()]["turns"] += 1
+
+func _handle_black_ball(team: String):
+	var winning_team: String
+	if $"..".get_tree().get_nodes_in_group(str("balls_", team)).size() < 1:
+		winning_team = team
+	else:
+		winning_team = Globals.turn_order[(Globals.current_turn_index + 1) % Globals.players.size()]["team"]
+	Globals.game_over = true
+	game_over.emit(winning_team)
+
 func _on_holes_point_scored(ball) -> void:
 	ball_dropped.emit(ball)
 	$"..".balls_group.erase(ball)
 	ball.remove_from_group("balls")
 	if ball.is_in_group("balls_A"):
 		ball.remove_from_group("balls_A")
-		if Globals.turn_order[Globals.current_turn_index]["team"] == "A":
-			Globals.players[Globals.current_turn_index]["score"] += 1
-			Globals.turn_order[Globals.current_turn_index]["turns"] += 1
-		else:
-			Globals.turn_order[(Globals.current_turn_index + 1) % Globals.players.size()]["turns"] += 1
+		_handle_points_and_turns("A")
 	elif ball.is_in_group("balls_B"):
 		ball.remove_from_group("balls_B")
-		if Globals.turn_order[Globals.current_turn_index]["team"] == "B":
-			Globals.players[Globals.current_turn_index]["score"] += 1
-			Globals.turn_order[Globals.current_turn_index]["turns"] += 1
-		else:
-			Globals.turn_order[(Globals.current_turn_index + 1) % Globals.players.size()]["turns"] += 1
-	else: #Balle noire
-		var winning_team : String
-		if $"..".get_tree().get_nodes_in_group("balls").size() == 0:
-			winning_team = Globals.turn_order[Globals.current_turn_index]["team"]
-		else:
-			winning_team = Globals.turn_order[(Globals.current_turn_index + 1) % Globals.turn_order.size()]["team"]
-		Globals.game_over = true
-		game_over.emit(winning_team)
+		_handle_points_and_turns("B")
+	else:
+		_handle_black_ball(Globals.turn_order[Globals.current_turn_index]["team"])
 	update_scoreboard.emit()
 	self.remove_child(ball)
 	$"../../../../GUI/Potted_balls_display".add_child(ball)
